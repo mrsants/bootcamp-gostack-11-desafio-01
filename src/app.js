1395e415525bd4fb77d5e2086e14580a10935eb1
Marcelo Santos <marcelobatipta@gmail.com>
2020-04-23 14:32:35 -0300

    Implementacao de desafio de conceitos de nodejs

--- src/app.js
+++ src/app.js
@@ -16,27 +16,78 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
-  const result = repositories.push({
+  const repository = {
     id: uuid(),
     title,
     url,
     techs,
     likes: 0,
-  });
+  };
 
-  return response.json(repositories[result-1]);
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryId === -1) {
+    return response.status(400).json([
+      {
+        error: "Repository does not exists.",
+      },
+    ]);
+  }
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryId].likes,
+  };
+
+  repositories[repositoryId] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryId >= 0) {
+    repositories.splice(repositoryId, 1);
+  } else {
+    return response.status(400).json({
+      error: "Repository does not exists",
+    });
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
